bc949cbe367a08aa46e16e0abd9020f4963b5ba5
Sam Townsend <contact@samt.io>
2018-12-13 17:01:19 -0800

    Update file Structure and add mapView

---
+++ VirtualTourist/Controller/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by Sam Townsend on 2018-12-13.
+//  Copyright © 2018 Sam Townsend. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+
+class MapViewController: UIViewController {
+    
+}
---
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Sam Townsend on 2018-12-13.
+//  Copyright © 2018 Sam Townsend. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+}
