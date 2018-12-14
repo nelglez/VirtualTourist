794054626b5a7a90fd7f19220acdfcc224dcb593
Sam Townsend <contact@samt.io>
2018-12-13 16:40:15 -0800

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Sam Townsend on 2018-12-13.
+//  Copyright © 2018 Sam Townsend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
